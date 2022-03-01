043c66b6fa0f3db4a674e8f39e0b4a2ba1944912
anaemerubio <anaemerubio@gmail.com>
2022-03-01 12:59:28 +0100

    Exercice 5

--- src/films.js
+++ src/films.js
@@ -3,7 +3,7 @@ function getAllDirectors(array) {
 
   let result = array.map((film) => film.director);
   
-  console.log("EXERCICE 1 ->", result);
+/*   console.log("EXERCICE 1 ->", result); */
   return result;
 
 }
@@ -13,7 +13,7 @@ function getMoviesFromDirector(array, director) {
 
   let result = array.filter((film) => film.director === director);
 
-  console.log("EXERCICE 2 ->", result);
+/*   console.log("EXERCICE 2 ->", result); */
   return result;
   
 }
@@ -46,34 +46,70 @@ function orderAlphabetically(array) {
   let title = array.map((film) => film.title);
   let result = title.sort().slice(0, 20);
 
-  console.log("EXERCICE 4 ->", result);
+/*   console.log("EXERCICE 4 ->", result); */
   return result;
 
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
 
-/*   let title = array.map((film) => film.title);
-  let result = title.sort().slice(0, 20);
+  // spread
+  let filmsToOrder = [...array];
 
-  console.log("EXERCICE 4 ->", result);
-  return result; */
+  let result = filmsToOrder.sort((yearA, yearB) => {
+
+    // Order by year
+    if (yearA.year < yearB.year) {
+        return -1;
+
+    } else if (yearA.year > yearB.year) {
+        return 1;
+
+    // If year ==, compare title
+    } else {
+        if (yearA.title < yearB.title){
+            return -1;
+        } else {
+            return 1;
+        }
+
+    }
+
+  });
+
+/*   console.log("EXERCICE 5 ->", result); */
+  return result; 
 
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory() {
 
+  // code
+
+/*   console.log("EXERCICE 6 ->", result);
+  return result;  */
+
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
 
+    // code
+
+/*     console.log("EXERCICE 7 ->", result);
+    return result;  */
+
 }
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
+
+    // code
+
+/*     console.log("EXERCICE 8 ->", result);
+    return result;  */
   
 }
 
